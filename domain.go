56e3f1920124cbc4f7bf03fd661413241df4648e
Josh Alon <jalon@asu.edu>
2024-11-21 15:51:47 -0800

    updated gorm tags for ticket group struct

--- domain.go
+++ domain.go
@@ -70,7 +70,7 @@ type TicketGroup struct {
 	Id                 uint      `json:"id" gorm:"primaryKey"`
 	Name               string    `json:"name" gorm:"size:255"`
 	SharedInboxGroupId uint      `json:"shared_inbox_group_id" gorm:"index"`
-	TicketIds          []uint    `json:"ticket_ids"`
+	TicketIds          []uint    `json:"ticket_ids" gorm:"serializer:json"`
 	HighestPriorityId  uint      `json:"highest_priority_id"`
 	OldestTicketDate   time.Time `json:"oldest_ticket_date"`
 }
